2d0db50f0fa4a66ff7818ea1bdfabbb28a195250
[Aarush Lohani] <[aarush.lohani2@gmail.com]>
2026-04-04 23:40:44 +0530

    Updated frontend to point to live Render API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
     formData.append("true_genre", trueGenre);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/submit-feedback/", {
+      const response = await fetch("https://ai-audio-analyzer-lo7n.onrender.com/submit-feedback/", {
         method: "POST",
         body: formData,
       });
